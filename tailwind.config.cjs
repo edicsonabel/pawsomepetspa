821174f308dbc70f1daf353f170f6b72eda953e6
Edicson Abel <edicsonabelinfo@gmail.com>
2022-10-18 09:00:57 -0400

    👀 navbar mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.{html,js}'],
-  darkMode: 'class',
   theme: {
-    colors: {
-      pink: '#ea1889',
-      yellow: '#f4ef21',
-      black: '#000000',
-      white: '#ffffff'
-    },
     extend: {}
   },
   plugins: []
