399113881b17d940c3ef923d2de60c66fdc8b04f
Edicson Abel <edicsonabelinfo@gmail.com>
2022-10-19 00:22:46 -0400

    🚀 Animation with paw logo

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 import './style.css'
+import { $$, random } from './utils'
+
+const algo = $$('#icons>.animate-moving-background')
+const maxWidth = window.innerWidth
+const maxHeight = window.innerHeight
+
+algo.forEach($el => {
+  $el.style.setProperty('left', `${random({ min: 0, max: maxWidth - 96 })}px`)
+  $el.style.setProperty('bottom', `${random({ min: 0, max: maxHeight / 1.5 })}px`)
+  $el.style.setProperty('--delay', `${random({ min: 0, max: 6, float: true })}s`)
+})
